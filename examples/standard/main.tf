53d58aa418de4def2d5542f55e3d1967e9a261c0
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-03-25 13:15:54 -0400

    test: enables mutltipe NAT gateway

--- examples/standard/main.tf
+++ examples/standard/main.tf
@@ -31,8 +31,9 @@ module "standard" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
 
   enable_vpn_gateway = true
 
