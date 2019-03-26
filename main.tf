d2d7656006dd7064d726de4747f6afbbab1677d5
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-03-26 16:53:06 -0400

    fix: fixes max subnet size

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 locals {
-  max_subnet_length = "${max(length(var.private_subnets), length(var.elasticache_subnets), length(var.database_subnets), length(var.redshift_subnets))}"
+  max_subnet_length = "${max(length(var.private_subnets), length(var.private_extra_subnets), length(var.elasticache_subnets), length(var.database_subnets), length(var.redshift_subnets))}"
   nat_gateway_count = "${var.single_nat_gateway ? 1 : (var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length)}"
 
   vpc_id = "${element(concat(aws_vpc_ipv4_cidr_block_association.this.*.vpc_id, aws_vpc.this.*.id, list("")), 0)}"
