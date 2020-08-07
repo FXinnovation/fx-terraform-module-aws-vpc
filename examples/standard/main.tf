4768aab4ff4810259a485adbf1962b218828bbfc
Quentin Vallin <quentin.vallin@fxinnovation.com>
2020-08-07 12:52:49 -0400

    test: add assume role

--- examples/standard/main.tf
+++ examples/standard/main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region     = "eu-west-1"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::700633540182:role/Jenkins"
+    session_name = "FXTestSandbox"
+  }
 }
 
 data "aws_security_group" "default" {
